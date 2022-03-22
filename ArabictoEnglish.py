87683e488c73cbe41a5d9037ee52d1a635436755
Baggins <102102665+Bagginsssss@users.noreply.github.com>
2022-03-22 00:32:00 -0400

    Add files via upload

---
+++ ArabictoEnglish.py
@@ -0,0 +1,45 @@
+def mylittleproject():
+    import tkinter
+    from tkinter import ttk, OptionMenu
+    from translate import Translator
+
+    Screen = tkinter.Tk()
+    Screen.title("English to Arabic")
+    Screen.geometry("450x200")
+
+    InputLanguageChoice = tkinter.StringVar()
+    TranslateLanguageChoice = tkinter.StringVar()
+
+    LanguageChoices = {'Hindi', 'English', 'French', 'German', 'Spansih', 'Arabic'}
+    InputLanguageChoice.set('English')
+    TranslateLanguageChoice.set('Arabic')
+
+    def Translate():
+        translator = Translator(from_lang=InputLanguageChoice.get(), to_lang=TranslateLanguageChoice.get())
+        Translation = translator.translate(TextVar.get())
+        OutputVar.set(Translation)
+
+    InputLanguageChoiceMenu = OptionMenu(Screen, InputLanguageChoice, *LanguageChoices)
+    tkinter.Label(Screen, text="Choose a Language").grid(row=0, column=1)
+    InputLanguageChoiceMenu.grid(row=1, column=1)
+
+    NewLanguageChoiceMenu = OptionMenu(Screen, TranslateLanguageChoice, *LanguageChoices)
+    tkinter.Label(Screen, text="Translated Language").grid(row=0, column=3)
+    NewLanguageChoiceMenu.grid(row=1, column=3)
+
+    tkinter.Label(Screen, text="Enter Text").grid(row=2, column=0)
+    TextVar = tkinter.StringVar()
+    TextBox = tkinter.Entry(Screen, textvariable=TextVar).grid(row=2, column=1)
+
+    tkinter.Label(Screen, text="Translated Text").grid(row=2, column=2)
+    OutputVar = tkinter.StringVar()
+    TextBox = tkinter.Entry(Screen, textvariable=OutputVar).grid(row=2, column=3)
+
+    tkinter.Label(Screen, text="I love you Dana").grid(row=8, column=2)
+
+    # Button for calling function
+    B = tkinter.Button(Screen, text="Translate", command=Translate, relief=tkinter.GROOVE).grid(row=3, column=1, columnspan=3)
+
+    tkinter.mainloop()
+
+
---
+++ randomnumber.py
@@ -0,0 +1,36 @@
+import random
+
+the_number = random.randint(1, 100)
+
+user = int(input("What's the number? "))
+
+count = 1
+while user != the_number:
+
+    if user > the_number:
+     print ("Lower")
+
+    elif user < the_number:
+     print ("Higher")
+
+    if user := int(input("What's the number?")):
+        count += 1
+        if count == 10:
+            print('Im sorry, that is not the right number')
+            break
+    if user == the_number:
+        print("You guessed it!!, the number is", the_number, "and it only" \
+              " took you", count, "tries")
+
+
+
+
+
+
+
+
+
+
+
+
+
